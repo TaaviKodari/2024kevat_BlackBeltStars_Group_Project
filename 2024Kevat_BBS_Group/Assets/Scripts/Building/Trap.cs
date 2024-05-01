33dfe488b44aaab2257bc67d5588cf5e47a43341
MattiDragon <61198884+MattiDragon@users.noreply.github.com>
2024-05-01 12:40:33 +0300

    Clean up trap code (script no longer extends Building, render order fixed)

--- 2024Kevat_BBS_Group/Assets/Scripts/Building/Trap.cs
+++ 2024Kevat_BBS_Group/Assets/Scripts/Building/Trap.cs
@@ -1,25 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Trap : Building
+public class Trap : MonoBehaviour
 {
     public float damage;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.TryGetComponent<Enemy>(out var enemy)) enemy.Damage(damage);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        if (other.TryGetComponent<Enemy>(out var enemy))
+        {
+            enemy.Damage(damage);
+        }
     }
 }
