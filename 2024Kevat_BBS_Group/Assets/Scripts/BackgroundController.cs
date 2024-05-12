40c9fc9cb5c7dabf69286b86fd403afe17c83b15
MattiDragon <61198884+MattiDragon@users.noreply.github.com>
2024-05-12 16:14:07 +0300

    New background

--- 2024Kevat_BBS_Group/Assets/Scripts/BackgroundController.cs
+++ 2024Kevat_BBS_Group/Assets/Scripts/BackgroundController.cs
@@ -15,11 +15,13 @@ public class BackgroundController : MonoBehaviour
 
     void Update()
     {
+        var spriteSize = renderer.sprite.bounds.size;
+        
         var worldScreenHeight = camera.orthographicSize * 2f;
         var worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
 
         var vCamPos = vCam.position;
-        transform.position = new Vector3(Mathf.Floor(vCamPos.x), Mathf.Floor(vCamPos.y), transform.position.z);
-        renderer.size = new Vector2(Mathf.Ceil(worldScreenWidth) + 2, Mathf.Ceil(worldScreenHeight) + 2);
+        transform.position = new Vector3(Mathf.Floor(vCamPos.x / spriteSize.x) * spriteSize.x, Mathf.Floor(vCamPos.y / spriteSize.y) * spriteSize.y, transform.position.z);
+        renderer.size = new Vector2(Mathf.Ceil(worldScreenWidth) + 2 * spriteSize.x, Mathf.Ceil(worldScreenHeight) + 2 * spriteSize.y);
     }
 }
