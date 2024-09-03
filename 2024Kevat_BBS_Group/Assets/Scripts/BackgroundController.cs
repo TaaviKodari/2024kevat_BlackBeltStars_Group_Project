577457c0c4b155caa792250adc0e6f1f8393792a
MattiDragon <matias.koivikko@gmail.com>
2024-09-03 19:08:08 +0300

    Document BackgroundController

--- 2024Kevat_BBS_Group/Assets/Scripts/BackgroundController.cs
+++ 2024Kevat_BBS_Group/Assets/Scripts/BackgroundController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+// Handles moving the background to match the player position and window size
 public class BackgroundController : MonoBehaviour
 {
     [SerializeField] 
@@ -17,11 +18,15 @@ public class BackgroundController : MonoBehaviour
     {
         var spriteSize = renderer.sprite.bounds.size;
         
+        // Get the width and height of the screen in world space to know how much background we need
         var worldScreenHeight = camera.orthographicSize * 2f;
         var worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
 
         var vCamPos = vCam.position;
+        // Move the background to the correct location
+        // We use a trick of dividing by the sprite size, then flooring and then multiplying by the size again to make the background snap to a grid
         transform.position = new Vector3(Mathf.Floor(vCamPos.x / spriteSize.x) * spriteSize.x, Mathf.Floor(vCamPos.y / spriteSize.y) * spriteSize.y, transform.position.z);
+        // Set the size of the background to match the world-space size of the camera, with a bit of extra padding
         renderer.size = new Vector2(Mathf.Ceil(worldScreenWidth) + 2 * spriteSize.x, Mathf.Ceil(worldScreenHeight) + 2 * spriteSize.y);
     }
 }
