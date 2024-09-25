a2252be15ffdb3e9a64cb28cace76a7d72b5eabf
Leevi Tulijoki <leevi.tulijoki@outlook.com>
2024-09-25 15:28:11 +0300

    made gate change color when open

--- 2024Kevat_BBS_Group/Assets/Scripts/Building/Gate.cs
+++ 2024Kevat_BBS_Group/Assets/Scripts/Building/Gate.cs
@@ -6,11 +6,13 @@ public class Gate : MonoBehaviour
 {
     public float timeOpen;
     private Collider2D coll;
+    private SpriteRenderer rend;
 
     // Start is called before the first frame update
     void Start()
     {
         coll = GetComponent<Collider2D>();
+        rend = GetComponent<SpriteRenderer>();
     }
 
     void OnCollisionEnter2D(Collision2D other)
@@ -18,6 +20,7 @@ public class Gate : MonoBehaviour
         if (other.gameObject.GetComponent<PlayerController>() != null)
         {
             coll.enabled = false;
+            rend.color = new Color(rend.color.r, rend.color.g, rend.color.b, 0.5F);
             Invoke(nameof(CloseGate), timeOpen);
         }
     }
@@ -25,5 +28,6 @@ public class Gate : MonoBehaviour
     void CloseGate()
     {
         coll.enabled = true;
+        rend.color = new Color(rend.color.r, rend.color.g, rend.color.b, 1);
     }
 }
