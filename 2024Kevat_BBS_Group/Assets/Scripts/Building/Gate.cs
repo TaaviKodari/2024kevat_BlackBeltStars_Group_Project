d911de44670d1c3396938c698011e8675515ca3c
Leevi Tulijoki <leevi.tulijoki@outlook.com>
2024-10-01 17:32:39 +0300

    made walls connect to gates

--- 2024Kevat_BBS_Group/Assets/Scripts/Building/Gate.cs
+++ 2024Kevat_BBS_Group/Assets/Scripts/Building/Gate.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gate : MonoBehaviour
+public class Gate : MonoBehaviour, IConnectable
 {
     public float timeOpen;
     private Collider2D coll;
