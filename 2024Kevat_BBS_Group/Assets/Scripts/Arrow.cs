a4372898ca2fe761562461431f989668e148ef78
MattiDragon <matias.koivikko@gmail.com>
2024-10-23 16:24:40 +0300

    Fix arrow being able to hit multiple enemies

--- 2024Kevat_BBS_Group/Assets/Scripts/Arrow.cs
+++ 2024Kevat_BBS_Group/Assets/Scripts/Arrow.cs
@@ -10,6 +10,7 @@ public class Arrow : MonoBehaviour
     private float maxTime = 5;
 
     private float timeTraveled;
+    private bool hasHit;
     public GameObject Owner { private get; set; }
 
     private void OnEnable()
@@ -29,14 +30,17 @@ public class Arrow : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (hasHit) return;
         if (other.gameObject == Owner) return;
         if (other.TryGetComponent<Entity>(out var entity))
         {
             entity.Damage(damage);
+            hasHit = true;
             Destroy(gameObject);
         } 
         else if (other.TryGetComponent<Building>(out var building) && building.data.blocksProjectiles)
         {
+            hasHit = true;
             Destroy(gameObject);
         }
     }
